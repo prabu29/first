58e6d1fe7d285bc98ff47abbbf1ec365cf70da96
Kanmaniselvan <Kanmaniselvan@gmail.com>
2014-06-05 12:44:24 +0530

    first one

---
+++ c.rb
@@ -0,0 +1,16 @@
+a=[1,-1000,67.798,-9]
+a.push(0,10)
+
+a.each{|t| puts t if t>0}
+
+3.upto(10){|t| puts t}
+
+if 10>8
+	puts "greater"
+elsif puts "small"
+end
+y=0
+while y<=10
+	puts y 
+	y+=1
+end
